cb2b4dfa44318b9638c3e8b4faf0bc66408d095d
futo aoi <faoi0803@icloud.com>
2025-08-22 14:44:55 +0900

    [Update]gitignoreにプレハブフォルダーを追加

--- Assets/Sclipts/ItemData.cs
+++ Assets/Sclipts/ItemData.cs
@@ -3,9 +3,14 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "newItem", menuName = "Inventory/Item")]
 public class ItemData : ScriptableObject
 {
-    public string ItemName;
-    public Image UIImage;
-    public string description;
+    [SerializeField,Header("アイテム名")] private string _itemName;
+    [SerializeField,Header("イメージ")] private Image _uiImage;
+    [SerializeField,Header("説明")] private string _description;
+    [SerializeField,Header("値段")] private int _value;
 
-    public int value;
+    public string ItemName => _itemName;
+    public Image UIImage => _uiImage;
+    public string Description => _description;
+    public int Value => _value;
+
 }
