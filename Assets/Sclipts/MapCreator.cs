4b511ab9f1738e9a3b9b0aa54301547c6250e4c9
futo aoi <faoi0803@icloud.com>
2025-08-26 20:00:57 +0900

    [Add]マップ生成の機能を追加

---
+++ Assets/Sclipts/MapCreator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> MapDates;
+    [SerializeField] int _mapSize = 25;
+    [SerializeField] int _mapVertical = 3;
+    [SerializeField] int _mapHorizontal = 3;
+    void Start()
+    {
+        for (int i = 0; i < _mapHorizontal; i++)
+        {
+            for (int j = 0; j < _mapVertical; j++)
+            {
+                Instantiate(MapDates[0], new Vector3(i *  _mapSize, 0, j * _mapSize), Quaternion.identity);
+            }
+        }
+
+
+    }
+}
